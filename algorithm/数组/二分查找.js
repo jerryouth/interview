8d6df269f0977757155aec9d4e71f5aaec24dcf9
jerryouth <674443170@qq.com>
2022-02-15 10:07:18 +0800

    add

--- algorithm/数组/二分查找.js
+++ algorithm/数组/二分查找.js
@@ -25,3 +25,36 @@ binary_search(nums, target)
 
 
 
+
+
+var minSubArrayLen = function(target, nums) {
+    // let result = 0
+    // for (let i = 0; i< nums.length; i++) {
+    //     if (nums[i] >= target) return 1
+    //     let sum = nums[i]
+    //     for (let j = i+1; j < nums.length; j++) {
+    //         sum += nums[j]
+    //         if (sum >= target) {
+    //             result = result ? Math.min(result, j-i+1): j-i+1
+    //             break
+    //         }
+    //     }
+    // }
+    // return result
+    let result = Infinity
+    let sum = 0
+    let i = 0
+    let sublength = 0
+    for (let j = 0; j < nums.length; j++) {
+        sum += nums[j];
+        while (sum >= target) {
+            sublength =(j - i + 1)
+            result = result < sublength ? result : sublength
+            sum -= nums[i++]
+        }
+    }
+    return result === Infinity ? 0 : result
+};
+
+
+minSubArrayLen(7, [2,3,1,2,4,3])
